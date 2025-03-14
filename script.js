6feb1e03dbdf0eb55d61af5bed2988b4242ced0e
keshavnagar <keshavnagar887@gmail.com>
2025-03-15 01:35:19 +0530

    add responsive navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector("nav")
+let hamburger = document.querySelector(".hamburger")
+function toggleMenu(){
+    nav.classList.toggle("responsive-nav")
+    hamburger.classList.toggle("hamburgerToggle")
+}
+
