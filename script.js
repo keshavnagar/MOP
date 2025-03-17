6cdcdf5ceb76a77af3a453b0628dbbe251789776
keshavnagar <keshavnagar887@gmail.com>
2025-03-17 19:44:22 +0530

    add hidden feature in scroll effect in home page

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-let nav = document.querySelector("nav")
-let hamburger = document.querySelector(".hamburger")
-function toggleMenu(){
-    nav.classList.toggle("responsive-nav")
-    hamburger.classList.toggle("hamburgerToggle")
+let nav = document.querySelector("nav");
+let hamburger = document.querySelector(".hamburger");
+function toggleMenu() {
+  nav.classList.toggle("responsive-nav");
+  hamburger.classList.toggle("hamburgerToggle");
 }
 
+// chalo gayab karet hai is indicator ko
+
+const scrollIndicator = document.querySelector("#scroll-indicator");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    scrollIndicator.classList.add("hidden");
+  } else {
+    scrollIndicator.classList.remove("hidden");
+  }
+});
+
+
+// ab apan ek work karte hai ki jo about me me apan ne heading likhi hai unpe click karu to property hide and apply honi chaiye 
+
+const h1 = document.querySelector(".webDevH1")
+const p = document.querySelector("#webDevP")
+
+h1.addEventListener("click",()=>{
+
+
+    p.classList.toggle("webDevPtoggle")
+    
+
+})
