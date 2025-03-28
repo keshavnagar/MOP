1f69d17bf98a16a6c8ea840f586dce601705a4a7
keshavnagar <keshavnagar887@gmail.com>
2025-03-28 15:11:16 +0530

    add changes in for blue , h , index and practice , script and style files

--- script.js
+++ script.js
@@ -16,16 +16,51 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// ab apan ek work karte hai ki jo about me me apan ne heading likhi hai unpe click karu to property hide and apply honi chaiye
 
-// ab apan ek work karte hai ki jo about me me apan ne heading likhi hai unpe click karu to property hide and apply honi chaiye 
+const p1 = document.querySelector("#webDevP");
+const div = document.querySelector(".web-dev");
+p1.style.display = "none";
+div.addEventListener("mouseover", () => {
+  p1.style.display = "block";
+});
 
-const h1 = document.querySelector(".webDevH1")
-const p = document.querySelector("#webDevP")
+div.addEventListener("mouseout", () => {
+  p1.style.display = "none";
+});
 
-h1.addEventListener("click",()=>{
+const p2 = document.querySelector("#p2");
+const div2 = document.querySelector(".ncc");
 
+p2.style.display = "none";
+div2.addEventListener("mouseover", () => {
+  p2.style.display = "block";
+});
 
-    p.classList.toggle("webDevPtoggle")
-    
+div2.addEventListener("mouseout", () => {
+  p2.style.display = "none";
+});
 
-})
+const p3 = document.querySelector("#p3");
+const div3 = document.querySelector(".ai");
+
+p3.style.display = "none";
+div3.addEventListener("mouseover", () => {
+  p3.style.display = "block";
+});
+
+div3.addEventListener("mouseout", () => {
+  p3.style.display = "none";
+});
+
+const p4 = document.querySelector("#p4");
+const div4 = document.querySelector(".active");
+
+p4.style.display = "none";
+div4.addEventListener("mouseover", () => {
+  p4.style.display = "block";
+});
+
+div4.addEventListener("mouseout", () => {
+  p4.style.display = "none";
+});
